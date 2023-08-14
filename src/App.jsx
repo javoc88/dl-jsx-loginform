65ccb4559b37a4120d0679b739aecead70e70cd8
Javoc <javier.ochoa.77@gmail.com>
2023-08-14 18:47:20 -0400

    editado App.jsx se agrega div container para aplicar estilos

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Registro from "./components/Registro";
 
 function App() {
-  const [alert, setAlert] = useState({msg: "", color: ""})
-  return <Registro alert={alert} setAlert={setAlert}/>;
+  const [alert, setAlert] = useState({ msg: "", color: "" });
+  return (
+    <>
+      <div className="container">
+        <Registro alert={alert} setAlert={setAlert} />
+      </div>
+    </>
+  );
 }
 
 export default App;
