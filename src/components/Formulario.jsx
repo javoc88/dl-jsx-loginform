0759a9596757dc131c765b60cc37ce8e003d943a
Javoc <javier.ochoa.77@gmail.com>
2023-08-14 13:17:19 -0400

    creado esqueleto Formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,5 +1,75 @@
+import { useState } from "react";
+
 const Formulario = () => {
-  return <></>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const validateData = () => {
+    if (password != confirmPassword) {
+      //componente Alert - "Contraseñas no coinciden"
+    }
+    if (
+      name === "" ||
+      email === "" ||
+      password === "" ||
+      confirmPassword === ""
+    ) {
+      //componente Alert - "Completar todos los campos"
+    }
+
+    // componente Alert - "Cuenta creada exitosamente"
+    setName("");
+    setEmail("");
+    setPassword("");
+    setConfirmPassword("");
+  };
+
+  return (
+    <>
+      <form onSubmit={validateData}>
+        <div>
+          <input
+            type="text"
+            name="username"
+            placeholder="Nombre"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="email"
+            name="email"
+            placeholder="tucorreo@ejemplo.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            name="password"
+            placeholder="Ingresa tu contraseña"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            name="confirm password"
+            placeholder="Confirma tu contraseña"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </div>
+        <div>
+          <button type="submit">Registrarse</button>
+        </div>
+      </form>
+    </>
+  );
 };
 
 export default Formulario;
